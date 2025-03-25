f393c38454759404708387c98da68277d10a54cb
aryasalwan <157759654+aryasalwan@users.noreply.github.com>
2025-03-25 12:03:43 -0230

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded',function(){
-
     document.getElementById('DarkMode').addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.scripting.executeScript({
-                target: {tabId: tabs[0].id},
-                files: ['content.js']
-            });
-        });
+        console.log("button clicked")
+        var enabled;
+        chrome.storage.local.get("scriptEnabled", function(result){
+            if (result.scriptEnabled !== undefined) {
+                // Toggle the existing value
+                chrome.storage.local.set({scriptEnabled: !result.scriptEnabled});
+            } else {
+                // Set to true if it's not defined
+                chrome.storage.local.set({scriptEnabled: true});
+            }
+        })
     });
     });
+
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.scripting.executeScript({
+//         target: {tabId: tabs[0].id},
+//         files: ['content.js']
+//     });
+// });  

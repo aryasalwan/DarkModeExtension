b200fcd0b38f0daafd0500a1bc4c02d5c216be9c
aryasalwan <157759654+aryasalwan@users.noreply.github.com>
2025-03-25 12:04:00 -0230

    Update content.js

--- content.js
+++ content.js
@@ -1,26 +1,45 @@
-//First  run deterence
-function DarkMode(){
-  chrome.storage.local.get('scriptEnabled', (data) => {
-    if (data.scriptEnabled) {
-      console.log("script already enabled")}
-      else{
-  var cover = document.createElement('div');
-  let css = `
-      position: fixed;
-      pointer-events: none;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      background-color: white;
-      mix-blend-mode: difference;
-      z-index: 2324561;
-  `;
-  cover.setAttribute('style', css);
-  document.body.appendChild(cover);
-  let headings = document.getElementsByTagName('h1');
+chrome.storage.local.get("scriptEnabled", function (val) {
+  console.log(val.scriptEnabled);
+  DarkMode(val.scriptEnabled)
 
-  console.log('Found', headings.length, 'h1 elements on this page');}
-})};
+})
 
-DarkMode()
+function DarkMode(enabled) {
+  if (enabled) {
+    console.log("script already enabled");
+  } else {
+    console.log("script not already enabled");
+    var cover = document.createElement("div");
+    let css = `
+    position: fixed;
+    pointer-events: none;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
+    mix-blend-mode: difference;
+    z-index: 2324561;
+`;
+    cover.setAttribute("style", css);
+    document.body.appendChild(cover);
+    let headings = document.getElementsByTagName("h1");
+
+    console.log("Found", headings.length, "h1 elements on this page");
+  }
+};
+
+console.log("test test");
+
+
+
+chrome.storage.onChanged.addListener(function(changes){
+
+  DarkMode(changes.newValue)
+
+})
+
+
+  // chrome.storage.local.get("scriptEnabled", function (val) {
+  //   console.log("test");
+  //   console.log(val.scriptEnabled);
